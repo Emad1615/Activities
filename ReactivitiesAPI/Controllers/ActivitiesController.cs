5ed140c5b7d89838ea273bbf4fd3d56c878c1a05
Emad1615 <76161087+Emad1615@users.noreply.github.com>
2025-07-15 19:49:12 +0300

    firstCommit

---
+++ ReactivitiesAPI/Controllers/ActivitiesController.cs
@@ -0,0 +1,32 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetActivities")]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+        [HttpGet("GeActivity")]
+        public async Task<ActionResult<Activity>> GeActivity(Guid id)
+        {
+            var activity = await _context.Activities.FindAsync(id);
+            if (activity != null)
+                return activity;
+            else return NotFound();
+        }
+    }
+}
