d541dc5e7fa5b0da9e500c9f4f755e15caa434ca
Emad1615 <76161087+Emad1615@users.noreply.github.com>
2025-07-16 15:35:48 +0300

    clientpart2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,49 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import axios from "axios";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:5107/api/Activities/GetActivities")
-      .then((response) => response.json())
-      .then((data) => setActivities(data));
+    axios
+      .get<Activity[]>("http://localhost:5107/api/Activities/GetActivities")
+      .then((response) => setActivities(response.data));
     return () => {};
   }, []);
 
   return (
     <>
-      <h1>Reactivities</h1>
-      <ul>
+      <Typography
+        variant="h3"
+        fontWeight={600}
+        sx={{
+          color: "blue",
+        }}
+      >
+        Reactivities
+      </Typography>
+      <List>
         {activities.map((activity) => (
-          <li key={activity.id}>{activity.title}</li>
+          <ListItem key={activity.id}>
+            <ListItemText>
+              <ListItemText
+                slotProps={{
+                  primary: {
+                    color: "#333",
+                  },
+                  secondary: {
+                    color: "#3333",
+                  },
+                }}
+                primary={activity.title}
+                secondary={activity.category}
+              />
+            </ListItemText>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 }
