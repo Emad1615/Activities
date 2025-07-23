92a15c007de2b2be378a36cc8c2c942d77a9440a
Emad1615 <76161087+Emad1615@users.noreply.github.com>
2025-07-23 16:40:28 +0300

    Update ActivitySidebar.tsx

--- client/src/features/activity/details/ActivitySidebar.tsx
+++ client/src/features/activity/details/ActivitySidebar.tsx
@@ -1,5 +1,47 @@
-import React from "react";
+import { Avatar, Box, Paper, Typography } from "@mui/material";
 
 export default function ActivitySidebar() {
-  return <div>ActivitySidebar</div>;
+  const isHost = true;
+  const following = true;
+  return (
+    <Paper>
+      <Typography
+        sx={{
+          p: 1,
+          bgcolor: "primary.main",
+          textAlign: "center",
+          color: "white",
+          fontWeight: "400",
+        }}
+        variant="body1"
+        color="inherit"
+        children={"2 people is going"}
+      />
+      <Box display="flex" justifyContent={"space-between"} py={2} px={1}>
+        <Box display="flex" gap={0.5} alignItems={"center"}>
+          <Avatar
+            alt="bob"
+            sx={{
+              width: 45,
+              height: 45,
+            }}
+          />
+          <Typography
+            sx={{
+              fontWeight: "500",
+            }}
+            variant="subtitle2"
+            color="inherit"
+            children={"Emad Ismail Mohammed"}
+          />
+        </Box>
+        <Box
+          display="flex"
+          gap={0.5}
+          alignItems={"center"}
+          justifyContent={"center"}
+        ></Box>
+      </Box>
+    </Paper>
+  );
 }
