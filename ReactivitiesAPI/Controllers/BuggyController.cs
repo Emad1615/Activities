87973b244c467e2f60a996eae48066579dfd6fa9
Emad1615 <76161087+Emad1615@users.noreply.github.com>
2025-07-29 00:08:52 +0300

    clientValidationErrors

---
+++ ReactivitiesAPI/Controllers/BuggyController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+
+        [HttpGet("notFound")]
+        public ActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+        [HttpGet("badRequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest();
+        }
+        [HttpGet("ServerError")]
+        public ActionResult GetServerError()
+        {
+            throw new Exception("Server Error");
+        }
+        [HttpGet("unauthorised")]
+        public ActionResult GetUnauthorized()
+        {
+            return Unauthorized();
+        }
+    }
+}
---
+++ client/src/features/errors/TestErrors.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+export default function TestErrors() {
+  const [validationError, setValidationError] = useState<string[]>([]);
+
+  return <div>TestErrors</div>;
+}
