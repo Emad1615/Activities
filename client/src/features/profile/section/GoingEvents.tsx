ab34854d545a92b19d699d12d82f17a3468fc034
Emad1615 <76161087+Emad1615@users.noreply.github.com>
2025-10-28 16:42:23 +0300

    events

--- client/src/features/profile/section/GoingEvents.tsx
+++ client/src/features/profile/section/GoingEvents.tsx
@@ -1,3 +1,69 @@
+import { Box, Grid, Skeleton, Typography } from '@mui/material';
+import { useProfile } from '../../../lib/hooks/profile/useProfile';
+import { useParams } from 'react-router';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
+import MiniActivityCard from '../../../App/shared/components/MiniActivityCard';
+
 export default function GoingEvents() {
-  return <div>GoingEvents</div>;
+  const { id } = useParams();
+  const {
+    userActivitiesGroup,
+    fetchNextPage,
+    hasNextPage,
+    loadingUserProfileActivities,
+  } = useProfile(id);
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
+  if (loadingUserProfileActivities)
+    return (
+      <Grid spacing={2} container>
+        {Array.from({ length: 3 }).map((_, idx) => (
+          <Box key={idx} sx={{ width: 210, marginRight: 0.5, my: 5 }}>
+            <Skeleton variant="rectangular" width={210} height={118} />
+            <Skeleton />
+            <Skeleton width="60%" />
+          </Box>
+        ))}
+      </Grid>
+    );
+
+  return (
+    <Box sx={{ overflowY: 'auto', height: 375, p: 1 }}>
+      {userActivitiesGroup?.pages.map((page, index) => (
+        <>
+          {page.items.length === 0 && (
+            <Typography
+              variant="body2"
+              color="text.disabled"
+              textAlign={'center'}
+            >
+              No past activities not found...
+            </Typography>
+          )}
+          <Box
+            ref={index === userActivitiesGroup.pages?.length ? ref : null}
+            key={index}
+            sx={{
+              display: 'grid',
+              gridTemplateColumns: 'repeat(auto-fill, minmax(210px, 1fr))',
+              gap: 2,
+              justifyContent: 'center',
+              alignItems: 'flex-start',
+            }}
+          >
+            {page.items.map((activity, idx) => (
+              <MiniActivityCard key={idx} activity={activity} />
+            ))}
+          </Box>
+        </>
+      ))}
+    </Box>
+  );
 }
--- client/src/features/profile/section/PastEvents.tsx
+++ client/src/features/profile/section/PastEvents.tsx
@@ -1,8 +1,9 @@
-import { Box, Grid, Skeleton } from '@mui/material';
+import { Box, Grid, Skeleton, Typography } from '@mui/material';
 import { useProfile } from '../../../lib/hooks/profile/useProfile';
 import { useParams } from 'react-router';
 import { useInView } from 'react-intersection-observer';
 import { useEffect } from 'react';
+import MiniActivityCard from '../../../App/shared/components/MiniActivityCard';
 
 export default function PastEvents() {
   const { id } = useParams();
@@ -20,7 +21,7 @@ export default function PastEvents() {
       fetchNextPage();
     }
   }, [inView, hasNextPage, fetchNextPage]);
-  if (!loadingUserProfileActivities)
+  if (loadingUserProfileActivities)
     return (
       <Grid spacing={2} container>
         {Array.from({ length: 3 }).map((_, idx) => (
@@ -33,5 +34,36 @@ export default function PastEvents() {
       </Grid>
     );
 
-  return <Box sx={{ overflowY: 'auto', height: 375, p: 1 }}></Box>;
+  return (
+    <Box sx={{ overflowY: 'auto', height: 375, p: 1 }}>
+      {userActivitiesGroup?.pages.map((page, index) => (
+        <>
+          {page.items.length === 0 && (
+            <Typography
+              variant="body2"
+              color="text.disabled"
+              textAlign={'center'}
+            >
+              No past activities not found...
+            </Typography>
+          )}
+          <Box
+            ref={index === userActivitiesGroup.pages?.length ? ref : null}
+            key={index}
+            sx={{
+              display: 'grid',
+              gridTemplateColumns: 'repeat(auto-fill, minmax(210px, 1fr))',
+              gap: 2,
+              justifyContent: 'center',
+              alignItems: 'flex-start',
+            }}
+          >
+            {page.items.map((activity, idx) => (
+              <MiniActivityCard key={idx} activity={activity} />
+            ))}
+          </Box>
+        </>
+      ))}
+    </Box>
+  );
 }
